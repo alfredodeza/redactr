63662e9d2f025221f055262b5f378955bfda7f63
Alfredo Deza <alfredo@deza.pe>
2023-08-16 08:11:59 -0400

    make it an HTTP API

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,33 @@
 use regex::Regex;
-use std::collections::HashMap;
+use redactr::load_rule_configs;
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder, web};
 
-// Define a struct to hold rule configuration
-struct Rule {
-    pattern: String,
-    mapping: HashMap<String, String>,
-    count: usize,
-    placeholder: String,
-    comment: String,
+#[get("/")]
+async fn index() -> impl Responder {
+    let html = r#"<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Redactr</title>
+</head>
+<body>
+    <h1>Redactr</h1>
+    <p>Redactr is a microservice that redacts personal identifiable information (PII) from text.</p>
+    <p>It is a HTTP API that accepts a JSON payload with a text field and returns a JSON payload with a redacted_text field.</p>
+    <p>It is built with Rust and Actix Web.</p>
+    <p>Endpoints available:</p>
+    <ul>
+        <li>POST <a href="/redact">/redact</a></li>
+        <li>GET <a href="/health">/health</a></li>
+    </ul>
+</body>
+</html>"#;
+    HttpResponse::Ok().body(html)
 }
 
-impl Rule {
-    fn new(pattern: String, placeholder: String, comment: String) -> Self {
-        Rule {
-            pattern,
-            mapping: HashMap::new(),
-            count: 0,
-            placeholder,
-            comment,
-        }
-    }
-
-    fn on_match(&mut self, matched_text: &str) -> String {
-        println!("Redacting: {}", matched_text);
-        let redacted_match = self
-            .mapping
-            .entry(matched_text.to_string())
-            .or_insert_with(|| {
-                self.count += 1;
-                format!("{}{}", self.placeholder, self.count)
-            });
-        println!("Redacted result: {}", redacted_match);
-        redacted_match.clone()
-    }
-}
-
-// Load the rules from a configuration file (JSON)
-fn load_rule_configs() -> Vec<Rule> {
-    // load the regex-fules.json file to provide configs
-    let json = include_str!("../regex-rules.json");
-
-    // use serde to load the json file
-    use serde::Deserialize;
-    use serde_json::{Result, Value};
-    #[derive(Deserialize)]
-    struct JSONRuleConfig {
-        pattern: String,
-        placeholder: String,
-        comment: String,
-    }
-
-    let loaded_json: Vec<JSONRuleConfig> = serde_json::from_str(json).unwrap();
-
-    let mut rules: Vec<Rule> = Vec::new();
-    for rule in loaded_json {
-        rules.push(Rule::new(
-            format!(r"{}", rule.pattern),
-            rule.placeholder,
-            rule.comment,
-        ));
-    }
-    rules
-}
-
-fn main() {
+#[post("/redact")]
+async fn redact(input_text: web::Json<String>) -> impl Responder {
     let mut rules = load_rule_configs();
-
-    // Input text with proper names
-    let input_text = "John Doe works with Adam Smith, and Adam Smith is about to retire. He lives in 22 Main St, Marietta, CA 94101. His phone number is 415-555-1234. His email is john@example.com";
 
     // Apply the rules sequentially
     let mut redacted_text = input_text.to_string();
@@ -75,13 +35,23 @@ fn main() {
         let regex = Regex::new(rule.pattern.as_str()).unwrap();
         for captures in regex.captures_iter(&input_text) {
             let matched_text = captures.get(0).unwrap().as_str();
-            println!("Matched text: {} - Comment: {}", matched_text, rule.comment);
             let redacted_match = rule.on_match(matched_text);
             redacted_text = redacted_text.replace(matched_text, &redacted_match);
-            println!("Redacted text: {}", redacted_text);
         }
     }
 
-    // Print the redacted text
-    println!("Redacted text: {}", redacted_text);
+    // Return the redacted text
+    HttpResponse::Ok().body(redacted_text)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(redact)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
