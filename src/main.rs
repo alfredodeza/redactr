08a28001902552f6001b833304cabd1da88f507d
Alfredo Deza <alfredo@deza.pe>
2023-08-16 08:52:40 -0400

    Add a health check

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,64 @@
 use regex::Regex;
 use redactr::load_rule_configs;
 use actix_web::{get, post, App, HttpResponse, HttpServer, Responder, web};
+use psutil::memory;
+use serde::Serialize;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+
+// Health endpoint JSON
+#[derive(Serialize)]
+struct HealthCheck {
+name: String,
+    status: String,
+}
+
+#[derive(Serialize)]
+struct HealthStatus {
+    uptime: u64,
+    memory_usage: f32,
+    disk_usage: u64,
+    checks: Vec<HealthCheck>,
+}
+
+#[get("/health")]
+async fn health() -> impl Responder {
+    let mut checks = vec![];
+
+    // Check container uptime
+    let uptime = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+    checks.push(HealthCheck {
+        name: "Container uptime".to_string(),
+        status: format!("{} seconds", uptime),
+    });
+
+    // Check memory usage
+    let memory_usage = memory::virtual_memory().unwrap();
+    let memory = memory_usage.percent();
+    checks.push(HealthCheck {
+        name: "Memory usage".to_string(),
+        status: format!("{} %", memory.to_string()),
+    });
+
+    // Check disk usage
+    let disk_usage = psutil::disk::disk_usage("/").unwrap();
+    checks.push(HealthCheck {
+        name: "Disk usage".to_string(),
+        status: format!("{} bytes", disk_usage.total()),
+    });
+
+    // Return the checks as a JSON object
+    let health_status = HealthStatus {
+        uptime,
+        memory_usage: memory_usage.percent(),
+        disk_usage: disk_usage.total(),
+        checks,
+    };
+    HttpResponse::Ok().json(health_status)
+}
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -50,6 +108,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(index)
             .service(redact)
+            .service(health)
     })
     .bind("127.0.0.1:8080")?
     .run()
